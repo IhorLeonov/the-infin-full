e778fac656a74e6a0a1fe05cf943c1773f6cc09d
Ihor Leonov <ihorleonow@gmail.com>
2024-06-24 14:30:09 +0300

    FIX | issues

--- src/app/individuals/page.tsx
+++ src/app/individuals/page.tsx
@@ -23,7 +23,7 @@ export default async function IndividualsPage({}: IndividualsPageProps) {
           bottomTextAccent="You’re probably right."
           bottomTextFirst="Bosses don’t see enough of your work to truly appreciate your contributions (let alone bosses of bosses). Your colleagues all smile at you, but is what they say behind your back holding up your career advancement? Or, quite the opposite — you have the respect and admiration of the entire team but it never reaches management (especially when it’s time to give out those year-end bonuses). "
           bottomTextSecond="Isn’t it time to take control of all this? With The INFIN, every employee provides honest and constructive  ⎯  and anonymous  ⎯  feedback to each other in a dynamic way that shows who’s the hero (and who’s the slacker) of the office. You can finally build your reputation with the help of those who know you best. And take it anywhere you work. It’s like a team-reviewed performance resume that actually matters. "
-          imageSrc="/images/individuals-hero.png"
+          imageSrc="/images/Individuals/individuals-hero.png"
           imageMobileSrc="/images/Individuals/individuals-hero.png"
           imageAlt="man portrait"
         />
